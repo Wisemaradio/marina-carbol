8042df12eb1a1d8a0fb740b699a543e5b5343c67
Wisemaradio <wisema2@hotmail.com>
2026-04-09 11:25:35 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,40 +1,38 @@
 self.addEventListener('install', function(event) {
-    console.log('Service Worker instalado');
 
     event.waitUntil(
-        caches.open('radio-cache-v1').then(function(cache) {
+        caches.open('radio-cache-v2').then(function(cache) {
             return cache.addAll([
-                '/',
                 '/index.html',
                 '/logo.jpg',
                 '/banner.jpg'
             ]);
         })
     );
+
+    self.skipWaiting();
 });
 
 self.addEventListener('activate', function(event) {
-    console.log('Service Worker activado');
 
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
             return Promise.all(
                 cacheNames.map(function(cacheName) {
-                    if (cacheName !== 'radio-cache-v1') {
+                    if (cacheName !== 'radio-cache-v2') {
                         return caches.delete(cacheName);
                     }
                 })
             );
         })
     );
+
+    self.clients.claim();
 });
 
 self.addEventListener('fetch', function(event) {
 
-    // ❌ NO cachear el streaming
-    if (event.request.url.includes('/listen/')) {
-        return;
-    }
+    if (event.request.url.includes('/listen/')) return;
 
     event.respondWith(
         caches.match(event.request).then(function(response) {
